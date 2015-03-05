a9891690227614805c05998f092e60c61c3cd675
Justin Morgan <justin.l.morgan@gmail.com>
2015-03-05 13:50:44 -0600

    Issue #1: Fix mixin from augmenting lodash by using _.runInContext

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 "use strict";
 
 // Required node modules
-var _ = require("lodash");
+var _ = require("lodash").runInContext();
 
-module.exports = _.mixin(_, {
+module.exports = _.mixin({
   isMatrix: require("./src/isMatrix.js"),
   transpose: require("./src/transpose.js")
 });
